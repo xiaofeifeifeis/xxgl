d0b7ee6afa89e22fe48d38eea2f8347465c119a4
mengfei <t-mengfei.zhang@pcitc.com>
2019-01-31 16:55:58 +0800

    创建项目架构

---
+++ src/main/java/com/mf/demo/XxglApplication.java
@@ -0,0 +1,14 @@
+package com.mf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XxglApplication.class, args);
+	}
+
+}
+
